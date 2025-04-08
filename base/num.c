a971522e70244195b06c6128b79ad8b3112f24c9
itallodemesquita <itallodemesquita@outlook.com.br>
2025-04-08 20:16:42 -0300

    add: num code

---
+++ base/num.c
@@ -0,0 +1,24 @@
+/* Questão 5 - Lista V */
+
+#include <stdio.h>
+
+int main () {
+
+    int num1, num2;
+
+
+    printf("\n\n");
+
+    printf("Digite o numero 1: ");
+    scanf("%d", &num1);
+    printf("\n");
+    printf("Digite o numero 2: ");
+    scanf("%d", &num2);
+
+
+    printf("\n\nDobro do primeiro: %d.", num1 * 2);
+    printf("\n\nTriplo do segundo: %d.", num2 * 3);
+
+
+    return 0;
+}
